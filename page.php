eb83b6247ff89585e62057d52fc603010b62917b
elf <filonenko0406@gmail.com>
2019-08-16 10:49:51 +0800

    fix

--- page.php
+++ page.php
@@ -2,11 +2,11 @@
     <?php get_template_part('parts/template/top-block'); ?>
     <?php get_template_part('parts/template/services'); ?>
     <?php //get_template_part('parts/template/bottom-block'); ?>
-    <section>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-            the_content();
-        endwhile; else: ?>
-            <p>Sorry, no posts matched your criteria.</p>
-        <?php endif; ?>
-    </section>
+<!--    <section>-->
+<!--        --><?php //if ( have_posts() ) : while ( have_posts() ) : the_post();
+//            the_content();
+//        endwhile; else: ?>
+<!--            <p>Sorry, no posts matched your criteria.</p>-->
+<!--        --><?php //endif; ?>
+<!--    </section>-->
 <?php get_footer(); ?>
