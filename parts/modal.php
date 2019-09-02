5d1a286ffb1f66c4f2737ff2a4d4daef5148196b
elf <filonenko0406@gmail.com>
2019-09-02 11:49:51 +0800

    add contact form

--- parts/modal.php
+++ parts/modal.php
@@ -1,5 +1,5 @@
 <div class="modal">
-    <div class="form">
+    <!--<div class="form">
         <div class="close change_border-no_hover"><span class="change_fill"></span><span class="change_fill-no_hover"></span></div>
         <div class="title">Оставьте заявку и вам перезвонит
             менеджер нашей компании</div>
@@ -7,5 +7,6 @@
         <input type="text" placeholder="Номер телефона" class="change_border-no_hover">
         <textarea placeholder="Сообщение" class="change_border-no_hover"></textarea>
         <input type="submit" class="change_border change_color">
-    </div>
+    </div>-->
+    <?php echo do_shortcode('[contact-form-7 id="239" title="Основная форма"]')?>
 </div>
