2cddb2ab58bb9a53de3a526d0fe8175cfa71783f
elf <filonenko0406@gmail.com>
2019-09-14 13:30:25 +0800

    add new yandex counter

--- footer.php
+++ footer.php
@@ -11,14 +11,14 @@
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(55145809, "init", {
+    ym(55324840, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
     });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/55145809" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/55324840" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 <script src="//cdn.callibri.ru/callibri.js" type="text/javascript" charset="utf-8"></script>
 <?php wp_footer(); ?>
