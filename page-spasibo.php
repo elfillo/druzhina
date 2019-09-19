c35497d656e4458af0424090b68d22426567c192
elf <filonenko0406@gmail.com>
2019-09-19 21:34:22 +0800

    fix thank page

--- page-spasibo.php
+++ page-spasibo.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
     <div class="container">
-        <section>
+        <section class="thanks_wrapper">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
                 the_content();
             endwhile; else: ?>
