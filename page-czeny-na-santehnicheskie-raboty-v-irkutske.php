c9b97d33ee475498bc81dd891d28c4a37743e2e7
elf <filonenko0406@gmail.com>
2019-10-05 21:25:28 +0800

    add detail page for price

---
+++ page-czeny-na-santehnicheskie-raboty-v-irkutske.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+    <div class="container">
+        <section class="thanks_wrapper">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                the_content();
+            endwhile; else: ?>
+                <p>Sorry, no posts matched your criteria.</p>
+            <?php endif; ?>
+        </section>
+        <br />
+    </div>
+<?php get_footer(); ?>
